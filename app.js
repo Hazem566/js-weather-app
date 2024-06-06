014aca485eee771a9ff36a83ac6eea940af581fe
HAZEM <haznmhmwd535@gmail.com>
2024-06-06 08:40:40 +0300

    finish html css part

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 
+const searchBox = document.getElementById("search__box");
+const searchBtn = document.getElementById("search__btn");
 
+const tempEl = document.querySelector(".temp");
+const img = document.querySelector(".temp__box img");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
 
 
 
